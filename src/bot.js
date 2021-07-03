0322bc42bf74a35f6870cff62759665666b94de0
Liamkoneko <38370861+Liamkoneko@users.noreply.github.com>
2021-07-03 22:18:42 +0200

    Version 2

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
+const fs = require("fs");
+
 const { Client } = require('discord.js');
+const { waitForDebugger } = require('inspector');
 const client = new Client();
 const prefix = "BIC//";
 
@@ -25,8 +28,21 @@ client.on('message', (message) => {
         if (CMD_NAME === 'testreply') {
             message.reply(`the reply test is succeeded if you can see this.`)
         }
+        if (CMD_NAME === 'analyzefile>>242426.wad') {
+            message.reply('```Request understood. Checking file...```')
+            message.channel.send('```INTELCOM SYSTEMS\n\nANALYSIS DONE! DISPLAYING RESULTS:\nFILESIZE: 14MB\nJUNKDATA SIZE: 13MB\nRECOVERABLE DATA: YES\nRECOVERABLE DATA TYPE: TEXT\nRECOVERABLE DATA TEXT: "Genata have been the ones responsible apparently for giving the blueprints of the 1000lb bomb to the PoD, Genata wanted the PoD to sell this data to dark corporations around the world, but we came back in time and Liamkoneko with Virt deleted the files from the servers, we thought it was the end, but Genata wanted these files back, since their copies seem to have been deleted by a defective agent which wanted to be back free, and which, from my darkweb files, is currently located in the Canada. So yeah, thats basically it. We`ll see more later down the road"```')
+        }
         if (CMD_NAME === 'latestmissionscheduled') {
-            message.reply('```INTELCOM SYSTEMS\n\nMISSION NAME: ZERO-CONTACT\nTASKED DIVISION: TFB\nTIME OF MISSION: 07:00 AM UTC-09:00 / 4:00 PM BST\nMISSION ORDER: FIND OUT WHAT HAPPENED IN THE SAFEHOUSE NEAR OUTPOST 114 AND ITS SURROUNDINGS.```')
+            const extractfsstringname = fs.readFileSync('./src/missionmessagesstoragemissionname.txt', 'utf-8');
+            const extractfsstringtime = fs.readFileSync('./src/missionmessagesstoragemissiontime.txt', 'utf-8');
+            const extractfsstringtasked = fs.readFileSync('./src/missionmessagesstoragetaskeddiv.txt', 'utf-8');
+            const extractfsstringdesc = fs.readFileSync('./src/missionmessagesstoragedesc.txt', 'utf-8');
+            console.log(extractfsstringname)
+            console.log(extractfsstringtime)
+            console.log(extractfsstringtasked)
+            console.log(extractfsstringdesc)
+            message.reply('')
+            message.channel.send('```INTELCOM SYSTEMS - MISSION VIEWER' + '\nMISSION NAME: ' + extractfsstringname + '\nTASKED DIVISION: ' + extractfsstringtasked + '\nOPERATION TIME: ' + extractfsstringtime + '\nMISSION ORDER ' + extractfsstringdesc)
         }
         if (CMD_NAME === 'searchpersonnelid') {
             messsage.reply('```INTELCOM SYSTEMS\n\nUNABLE TO INITIATE COMMAND, STILL WORK IN PROGRESS. SHUTTING DOWN REQUEST!```')
